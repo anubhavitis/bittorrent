30918bfccc1877b5a2ba7914b237b8bd65e2c6ba
Anubhav Singhal <ss.lfsgd@gmail.com>
2025-04-25 05:14:34 +0530

    refactored bencode into new file

--- src/main.rs
+++ src/main.rs
@@ -1,73 +1,5 @@
-use serde_json::{self, json};
 use std::env;
-
-fn decode_bencoded_string(encoded_value: &str) -> (serde_json::Value, usize) {
-    let colon_index = encoded_value.find(':').unwrap();
-    let length = encoded_value[..colon_index].parse::<usize>().unwrap();
-    let string_start = colon_index + 1;
-    let string_end = string_start + length;
-    let string = &encoded_value[string_start..string_end];
-
-    (serde_json::Value::String(string.to_string()), string_end)
-}
-
-fn decode_bencoded_number(encoded_value: &str) -> (serde_json::Value, usize) {
-    // Format: i<number>e
-    let e_index = encoded_value.find('e').unwrap();
-    let number_string = &encoded_value[1..e_index]; // Skip the 'i' prefix
-    let number = number_string.parse::<i64>().unwrap();
-
-    (serde_json::Value::Number(number.into()), e_index + 1)
-}
-
-fn decode_bencoded_list(encoded_value: &str) -> (serde_json::Value, usize) {
-    // Format: l<contents>e
-    let mut current_index = 1; // Skip the 'l' prefix
-    let mut values = Vec::new();
-
-    while encoded_value.chars().nth(current_index).unwrap() != 'e' {
-        let (value, value_len) = decode_bencoded_value(&encoded_value[current_index..]);
-        values.push(value);
-        current_index += value_len;
-    }
-
-    (serde_json::Value::Array(values), current_index + 1) // +1 for the 'e' suffix
-}
-
-fn decode_bencoded_dict(encoded_value: &str) -> (serde_json::Value, usize) {
-    // Format: d<contents>e
-    let mut current_index = 1; // Skip the 'd' prefix
-    let mut key_values = vec![];
-
-    while encoded_value.chars().nth(current_index).unwrap() != 'e' {
-        let (key, key_len) = decode_bencoded_string(&encoded_value[current_index..]);
-        let (value, value_len) = decode_bencoded_value(&encoded_value[current_index + key_len..]);
-        key_values.push(format!("{}: {}", key, value));
-        current_index += key_len + value_len;
-    }
-
-    let json_string = format!("{{{}}}", key_values.join(","));
-    let dict_elem = serde_json::from_str::<serde_json::Value>(&json_string).unwrap();
-
-    eprintln!("dict_values: {:?}", dict_elem);
-    let resp = serde_json::Value::Object(dict_elem.as_object().unwrap().clone());
-    eprintln!(" resp {}", resp);
-
-    (resp, current_index + 1) // +1 for the 'e' suffix
-}
-
-#[allow(dead_code)]
-fn decode_bencoded_value(encoded_value: &str) -> (serde_json::Value, usize) {
-    match encoded_value.chars().next().unwrap() {
-        '0'..='9' => decode_bencoded_string(encoded_value),
-        'i' => decode_bencoded_number(encoded_value),
-        'l' => decode_bencoded_list(encoded_value),
-        'd' => decode_bencoded_dict(encoded_value),
-        _ => panic!("Unhandled encoded value: {}", encoded_value),
-    }
-}
-
-// Usage: your_bittorrent.sh decode "<encoded_value>"
+mod bencode;
 fn main() {
     let args: Vec<String> = env::args().collect();
 
@@ -84,7 +16,7 @@ fn main() {
 
         // Uncomment this block to pass the first stage
         let encoded_value = &args[2];
-        let (decoded_value, _) = decode_bencoded_value(encoded_value);
+        let (decoded_value, _) = bencode::decode_bencoded_value(encoded_value);
         println!("{}", decoded_value.to_string());
     } else {
         println!("unknown command: {}", args[1])
