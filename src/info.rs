7946faf1e1070dbaf53a5dab85c4c5b3c589d8d8
Anubhav Singhal <ss.lfsgd@gmail.com>
2025-04-25 15:21:39 +0530

    able to hash the info

--- src/info.rs
+++ src/info.rs
@@ -1,5 +1,6 @@
 use serde::{Deserialize, Serialize};
 use serde_bytes::ByteBuf;
+use sha1::{Digest, Sha1};
 
 #[derive(Debug, Serialize, Deserialize)]
 struct Torrent {
@@ -21,4 +22,10 @@ pub fn get_info(file_name: &std::path::PathBuf) {
     let torrent: Torrent = serde_bencode::from_bytes(&file).unwrap();
     println!("Tracker URL: {}", torrent.announce);
     println!("Length: {}", torrent.info.length);
+
+    let mut hasher = Sha1::new();
+    let info_bytes = serde_bencode::to_bytes(&torrent.info).unwrap();
+    hasher.update(&info_bytes);
+    let hash = hasher.finalize();
+    println!("Info Hash: {}", hex::encode(hash));
 }
