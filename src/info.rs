690a6d57ed35371056449b4227129c3f67556758
Anubhav Singhal <ss.lfsgd@gmail.com>
2025-04-25 15:30:15 +0530

    got the piece hashes

--- src/info.rs
+++ src/info.rs
@@ -28,4 +28,21 @@ pub fn get_info(file_name: &std::path::PathBuf) {
     hasher.update(&info_bytes);
     let hash = hasher.finalize();
     println!("Info Hash: {}", hex::encode(hash));
+
+    println!("Piece Length: {}", torrent.info.piece_length);
+    println!("Piece Hashes:");
+    let hashes = get_piece_hashes(&torrent.info.pieces);
+    for hash in hashes {
+        println!("{}", hash);
+    }
+}
+
+fn get_piece_hashes(pieces: &ByteBuf) -> Vec<String> {
+    let mut hashes = Vec::new();
+    for i in 0..pieces.len() / 20 {
+        let hash = pieces[i * 20..(i + 1) * 20].to_vec();
+        hashes.push(hex::encode(hash));
+    }
+
+    hashes
 }
